0cc33d3a09af466ef56c492559b4c1c65b0696fc
Mateus Castro <mateuslopescastro@icloud.com>
2023-07-28 01:33:20 -0300

    feat: app theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+
+import { AppTheme } from './styles/global'
+import { theme } from './styles/theme'
+
 export function App() {
-  return <span>ignite-react-js-project-timer</span>
+  return (
+    <ThemeProvider theme={theme}>
+      <span>ignite-react-js-project-timer</span>
+
+      <AppTheme />
+    </ThemeProvider>
+  )
 }
