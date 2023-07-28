ec6a46d102c19f15195b31aec603d1f75d73e41c
Mateus Castro <mateuslopescastro@icloud.com>
2023-07-28 16:53:29 -0300

    feat: history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,48 @@
+import { Container, Status, TableContainer } from './styles'
+
 export function History() {
-  return <span>History</span>
+  return (
+    <Container>
+      <h1>Meu histórico</h1>
+
+      <TableContainer>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>10 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 2</td>
+              <td>10 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 3</td>
+              <td>10 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluída</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </TableContainer>
+    </Container>
+  )
 }
