476edb0057399b27d9c9a66c3eda950c7d521642
Mateus Castro <mateuslopescastro@icloud.com>
2023-07-28 02:31:57 -0300

    feat: home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,46 @@
+import { Play } from 'phosphor-react'
+
+import {
+  Container,
+  CountdownContainer,
+  FormHeader,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
 export function Home() {
-  return <span>Home</span>
+  return (
+    <Container>
+      <form>
+        <FormHeader>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            placeholder="00"
+            type="number"
+          />
+
+          <span>minutos.</span>
+        </FormHeader>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          Começar
+          <Play size={24} />
+        </StartCountdownButton>
+      </form>
+    </Container>
+  )
 }
