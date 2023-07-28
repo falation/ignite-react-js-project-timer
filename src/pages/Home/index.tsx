40030bd70dc444951bd21085ff7e7a7ab5cd4ab5
Mateus Castro <mateuslopescastro@icloud.com>
2023-07-28 02:38:01 -0300

    feat(timer): form inputs design

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,12 +16,27 @@ export function Home() {
       <form>
         <FormHeader>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
+
           <MinutesAmountInput
             id="minutesAmount"
+            min={5}
+            max={60}
             placeholder="00"
+            step={5}
             type="number"
           />
 
