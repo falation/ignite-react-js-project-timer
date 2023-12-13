679d62a5dd93e5b38a5fab74f1b8483757988bfe
Mateus Castro <mateuslopescastro@icloud.com>
2023-12-12 23:45:51 -0300

    feat(timer): countdown in tab title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -86,6 +86,12 @@ export function Home() {
   const minutes = minutesAmount.toString().padStart(2, '0')
   const seconds = secondsAmount.toString().padStart(2, '0')
 
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [activeCycle, minutes, seconds])
+
   const task = watch('task')
   const isSubmitDisabled = !task
 
